3a82946a1fc78a24df65362afb78e2e3355e70db
MHSons <muhammadasadhanif786@gmail.com>
2025-09-22 14:31:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// site-wide helper functions (client-side)
+console.log('Alpha Lab script loaded');
 
-// General site JS placeholder
-console.log('site script loaded');
+function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('lab_user') || 'null');
+}
+
+function requireLogin() {
+  const user = getCurrentUser();
+  if (!user) {
+    alert('You must be logged in to access this page.');
+    window.location = 'login.html';
+  }
+  return user;
+}
